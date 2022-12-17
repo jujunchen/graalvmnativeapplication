8acbe7872e1477ad819c0552a6b08e6ec0447d27
阿提说说 <0431cjj@163.com>
2022-12-17 14:00:47 +0800

    First GraalVM Native Application

---
+++ src/test/java/com/example/graalvmnativeapplication/GraalvmNativeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graalvmnativeapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraalvmNativeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
